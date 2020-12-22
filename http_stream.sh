5c7b47483c4596202549dd25ee5dde906d0ebd2b
Mike Splain <mike.splain@gmail.com>
2020-12-22 17:50:15 -0500

    Additions

--- http_stream.sh
+++ http_stream.sh
@@ -7,4 +7,4 @@ RTSP_PASSWORD=${RTSP_PASSWORD:-password}
 RTSP_ADDRESS=${RTSP_ADDRESS:-127.0.0.1}
 RTSP_PATH=${RTSP_PATH:-/live}
 
-cvlc -I dummy rtsp://${RTSP_USER}:${RTSP_PASSWORD}@${RTSP_ADDRESS}${RTSP_PATH} --sout "#transcode{vcodec=mjpg,quality=3,fps=5,acodec=none,width=1280,height=720}:standard{access=http{mime=multipart/x-mixed-replace; boundary=7b3cc56e5f51db803f790dad720ed50a},mux=mpjpeg,dst=:8888/videostream.cgi}"
+cvlc -I dummy rtsp://${RTSP_USER}:${RTSP_PASSWORD}@${RTSP_ADDRESS}${RTSP_PATH} --sout-x264-preset fast --sout "#transcode{vcodec=mjpg,quality=3,fps=5,acodec=none,width=1280,height=720}:standard{access=http{mime=multipart/x-mixed-replace; boundary=7b3cc56e5f51db803f790dad720ed50a},mux=mpjpeg,dst=:8888/videostream.cgi}" --sout-keep
